b33a17243643f1817cfff1c8aed94cf216dff70e
AFCMS <afcm.contact@gmail.com>
2026-03-19 18:53:24 +0100

    Better TierList UI

--- src/components/TierItem.tsx
+++ src/components/TierItem.tsx
@@ -1,12 +1,18 @@
-export function TierItem() {
-  const image = undefined;
-  const name = "AAA";
+export interface TierItemProps {
+  readonly name: string;
+  readonly image?: string;
+}
 
-  return image ? (
-    <img src={image} alt="Tier item" className="size-20 rounded object-cover" />
+export function TierItem(props: TierItemProps) {
+  return props.image ? (
+    <img
+      src={props.image}
+      alt="Tier item"
+      className="size-20 rounded object-cover"
+    />
   ) : (
     <div className="flex size-20 items-center justify-center rounded bg-zinc-300">
-      {name}
+      {props.name}
     </div>
   );
 }
--- src/components/TierList.tsx
+++ src/components/TierList.tsx
@@ -1,47 +1,85 @@
+import { useState } from "react";
 import { TierItem } from "./TierItem";
+import type { Ranking } from "./ItemRankings";
 
 export function TierList() {
+  // const editable = false;
+
+  const [items] = useState<{
+    [key in Ranking | "POOL"]: string[];
+  }>({
+    S: ["S0", "S1", "S2"],
+    A: ["A0", "A1", "A2"],
+    B: ["B0", "B1"],
+    C: [],
+    D: ["D0"],
+    POOL: ["POOL0", "POOL1", "POOL2", "POOL3"],
+  });
+
   return (
-    <div className="grid max-w-3xl grid-cols-[5rem_1fr] gap-0.5 bg-zinc-200">
-      <div
-        className="flex size-20 items-center justify-center"
-        data-rank-bg="S"
-      >
-        S
+    <>
+      <div className="grid max-w-3xl grid-cols-[5rem_1fr] gap-0.5 bg-zinc-200">
+        <div
+          className="flex size-20 items-center justify-center"
+          data-rank-bg="S"
+        >
+          S
+        </div>
+        <div className="flex flex-wrap gap-0.5">
+          {items.S.map((item) => (
+            <TierItem key={item} name={item} />
+          ))}
+        </div>
+        <div
+          className="flex size-20 items-center justify-center"
+          data-rank-bg="A"
+        >
+          A
+        </div>
+        <div className="flex flex-wrap gap-0.5">
+          {items.A.map((item) => (
+            <TierItem key={item} name={item} />
+          ))}
+        </div>
+        <div
+          className="flex size-20 items-center justify-center"
+          data-rank-bg="B"
+        >
+          B
+        </div>
+        <div className="flex flex-wrap gap-0.5">
+          {items.B.map((item) => (
+            <TierItem key={item} name={item} />
+          ))}
+        </div>
+        <div
+          className="flex size-20 items-center justify-center"
+          data-rank-bg="C"
+        >
+          C
+        </div>
+        <div className="flex flex-wrap gap-0.5">
+          {items.C.map((item) => (
+            <TierItem key={item} name={item} />
+          ))}
+        </div>
+        <div
+          className="flex size-20 items-center justify-center"
+          data-rank-bg="D"
+        >
+          D
+        </div>
+        <div className="flex flex-wrap gap-0.5">
+          {items.D.map((item) => (
+            <TierItem key={item} name={item} />
+          ))}
+        </div>
       </div>
-      <div className="flex flex-wrap">
-        {[1, 2, 3, 4, 5].map((item) => (
-          <TierItem key={item} />
+      <div className="mt-4 flex w-full flex-wrap gap-0.5">
+        {items.POOL.map((item) => (
+          <TierItem key={item} name={item} />
         ))}
       </div>
-      <div
-        className="flex size-20 items-center justify-center"
-        data-rank-bg="A"
-      >
-        A
-      </div>
-      <div>AAA</div>
-      <div
-        className="flex size-20 items-center justify-center"
-        data-rank-bg="B"
-      >
-        B
-      </div>
-      <div>AAA</div>
-      <div
-        className="flex size-20 items-center justify-center"
-        data-rank-bg="C"
-      >
-        C
-      </div>
-      <div>AAA</div>
-      <div
-        className="flex size-20 items-center justify-center"
-        data-rank-bg="D"
-      >
-        D
-      </div>
-      <div>AAA</div>
-    </div>
+    </>
   );
 }
