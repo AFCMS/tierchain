3461d1556bc510ae5c8e23e20c9f0b9be11112f4
AFCMS <afcm.contact@gmail.com>
2026-03-19 12:20:34 +0100

    Initial commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+/**
+ * This is a hack to make vite-imagetools work with typescript.
+ */
+declare module "*&imagetools" {
+  const out: string;
+  export default out;
+}
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SEPOLIA_RPC_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
